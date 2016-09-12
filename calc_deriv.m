058cb6935d6cb3c8626295f76654d41c6f660340
Leo Darengosse <leo.darengosse@hotmail.fr>
2016-09-12 16:02:22 +0200

    correction_deriv

--- calc_deriv.m
+++ calc_deriv.m
@@ -1,7 +1,7 @@
 function [dr_dx, dr_dy, dr_dz] = calc_deriv(Point,Sat)
 
-    dr_dx = (Point.X-Sat.X)/sqrt(dist_cart(Point,Sat));
-    dr_dy = (Point.Y-Sat.Y)/sqrt(dist_cart(Point,Sat));
-    dr_dz = (Point.Z-Sat.Z)/sqrt(dist_cart(Point,Sat));
+    dr_dx = (Point.X-Sat.X)/dist_cart(Point,Sat);
+    dr_dy = (Point.Y-Sat.Y)/dist_cart(Point,Sat);
+    dr_dz = (Point.Z-Sat.Z)/dist_cart(Point,Sat);
     
     
